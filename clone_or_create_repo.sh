27f02dadb3ab497d38a4be4226cacbb9ad1ca992
Bobligity <Bobligity@gmail.com>
2021-01-31 00:31:06 -0500

    debugging create repo script

--- clone_or_create_repo.sh
+++ clone_or_create_repo.sh
@@ -15,7 +15,7 @@ else
     git add README.md
     git commit -m "first commit"
     git branch -M main
-    git remote add origin https://github.com/Nowhere-Know-How/$REPO_NAME.git
+    git remote add origin https://nwkhteam:$GITHUB_TOKEN@github.com/Nowhere-Know-How/$REPO_NAME.git
     git push -u origin main
     cd ..
     echo "Repo has been created"
