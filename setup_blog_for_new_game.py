4b4551eeb9a1b529a504e758adbf9477f5ce2ef2
Bobligity <Bobligity@gmail.com>
2021-02-06 18:54:13 -0500

    adding permalink to latest release

--- setup_blog_for_new_game.py
+++ setup_blog_for_new_game.py
@@ -3,12 +3,13 @@ import os
 import sys
 
 CWD = os.getcwd()
-input = sys.argv[1]
+game_name = sys.argv[1]
+org_name = sys.argv[2]
 game_ready = False
 about_item_index = -1
 
 def create_menu_item():
-    new_menu_item = {"title": input, "url": f"/{input.lower()}/"}
+    new_menu_item = {"title": game_name, "url": f"/{game_name.lower()}/"}
     about_item = config["menu"].pop(about_item_index)
     config["menu"].append(new_menu_item)
     config["menu"].append(about_item)
@@ -20,22 +21,23 @@ def create_menu_item():
 def create_markdown_from_template():
     with open(os.path.join(CWD, "_templates", "new_game.md"), "r") as file:
         markdown_template = file.read()
-        markdown_file = markdown_template.replace("$TITLE", input)
+        markdown_file = markdown_template.replace("$TITLE", game_name)
+        markdown_file = markdown_template.replace("$ORG", org_name)
 
-    with open(os.path.join(CWD, "_featured_categories", f"{input}.md"), "w") as file:
+    with open(os.path.join(CWD, "_featured_categories", f"{game_name}.md"), "w") as file:
         file.write(markdown_file)
 
 
 def create_post_directory():
     os.umask(0)
-    os.mkdir(os.path.join(CWD, "_posts", f"{input}"), 777)
+    os.mkdir(os.path.join(CWD, "_posts", f"{game_name}"), 777)
 
 
 with open(os.path.join(CWD, "_config.yml")) as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
 
 for i in range(len(config["menu"])):
-    if config["menu"][i]["title"] == input:
+    if config["menu"][i]["title"] == game_name:
         game_ready = True
     if config["menu"][i]["title"] == "About":
         about_item_index = i
