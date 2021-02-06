84adcc5a7d3520ddef89abe7c95281a6721ed32b
Bobligity <Bobligity@gmail.com>
2021-02-06 18:28:39 -0500

    updating variable names

--- clone_or_create_repo.sh
+++ clone_or_create_repo.sh
@@ -2,13 +2,13 @@
 GITHUB_TOKEN=$1
 REPO_NAME=$2
 ORG_NAME=$3
-MACHINE_USER=$4
+GITHUB_USER=$4
 
-if git clone https://$MACHINE_USER:$GITHUB_TOKEN@github.com/$ORG_NAME/$REPO_NAME.git
+if git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$ORG_NAME/$REPO_NAME.git
 then 
     echo "Repo already exists."
 else 
-    git config --global user.name $MACHINE_USER
+    git config --global user.name $GITHUB_USER
     gh repo create $ORG_NAME/$REPO_NAME --public --confirm
     mkdir ./$REPO_NAME
     cd ./$REPO_NAME
@@ -17,7 +17,7 @@ else
     git add README.md
     git commit -m "first commit"
     git branch -M main
-    git remote add origin https://$MACHINE_USER:$GITHUB_TOKEN@github.com/$ORG_NAME/$REPO_NAME.git
+    git remote add origin https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$ORG_NAME/$REPO_NAME.git
     git push -u origin main
     cd ..
     echo "Repo has been created"
